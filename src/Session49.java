056d583b4701a2d12e24313ccd80da797a95ff50
priya <priyankadeshpande034@gmail.com>
2020-10-06 19:10:36 +0530

    Session49

--- src/Session49.java
+++ src/Session49.java
@@ -1,17 +1,26 @@
 public class Session49 {
     public static void main(String[] args) {
-        String str1 = new String("Durga");
-        String str2 = str1.concat("Software");
-        String str3 = str2.concat("Solutions");
+        String str1 = new String("Durga ");
+        String str2 = str1.concat("Software ");
+        String str3 = str2.concat("Solutions ");
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
         System.out.println();
-        StringBuffer sb1 = new StringBuffer("Durga");
-        StringBuffer sb2 = sb1.append("Software");
-        StringBuffer sb3 = sb2.append("Solutions");
+        StringBuffer sb1 = new StringBuffer("Durga ");
+        StringBuffer sb2 = sb1.append("Software ");
+        StringBuffer sb3 = sb2.append("Solutions ");
+
         System.out.println(sb1);
         System.out.println(sb2);
         System.out.println(sb3);
+        System.out.println();
+        System.out.println(str1 == str2);
+        System.out.println(str2 == str3);
+        System.out.println(str3 == str1);
+        System.out.println();
+        System.out.println(sb1 == sb2);
+        System.out.println(sb2 == sb3);
+        System.out.println(sb3 == sb1);
     }
 }

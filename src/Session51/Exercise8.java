2d4197b32424a3ecb02efe3e42a850d0c2264faa
priya <priyankadeshpande034@gmail.com>
2020-10-06 22:34:21 +0530

    Exercise8

---
+++ src/Session51/Exercise8.java
@@ -0,0 +1,15 @@
+package Session51;
+
+public class Exercise8 {
+    int i = 10;
+
+    Exercise8() {
+        System.out.println("A-Con");
+    }
+}
+
+class Test9 {
+    public static void main(String[] args) {
+        Exercise8 ex = new Exercise8();
+    }
+}

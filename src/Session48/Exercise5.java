123680eee5568586053a0d1d619ba270ad08da64
priya <priyankadeshpande034@gmail.com>
2020-10-06 14:38:48 +0530

    Exercise5

--- src/Session48/Exercise5.java
+++ src/Session48/Exercise5.java
@@ -4,5 +4,16 @@ public class Exercise5 {
     public static void main(String[] args) {
         String str = new String("Durga software solutions");
         System.out.println(str);
+        Exception ex = new Exception("My Own Exception");
+        System.out.println(ex);
+        Thread t = new Thread("Core Java");
+        System.out.println(t);
+        Integer in = new Integer (100);
+        java.util.ArrayList al = new java.util.ArrayList();
+        al.add("AAA");
+        al.add("BBB");
+        al.add("CCC");
+        al.add("DDD");
+        System.out.println(al);
     }
 }

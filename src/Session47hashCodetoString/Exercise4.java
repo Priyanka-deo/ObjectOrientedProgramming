f6111bc1947cda836e9002d9f6b157938dbe9a82
priya <priyankadeshpande034@gmail.com>
2020-10-06 09:17:39 +0530

    Exercise4

---
+++ src/Session47hashCodetoString/Exercise4.java
@@ -0,0 +1,11 @@
+package Session47hashCodetoString;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Exercise4 exe = new Exercise4();
+        String ref = exe.toString();
+        System.out.println(ref);
+        System.out.println(args.toString());
+        System.out.println(exe);
+    }
+}

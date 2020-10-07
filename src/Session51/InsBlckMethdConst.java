e80a85dec1c2ec56636d8db27547ee9b04163b97
priya <priyankadeshpande034@gmail.com>
2020-10-07 19:06:47 +0530

    InsBlckMethdConst

---
+++ src/Session51/InsBlckMethdConst.java
@@ -0,0 +1,19 @@
+package Session51;
+
+public class InsBlckMethdConst {
+    {
+        System.out.println("IB-A");
+    }
+
+    InsBlckMethdConst() {
+        System.out.println("A-Con");
+    }
+}
+
+class Test11 {
+    public static void main(String[] args) {
+        InsBlckMethdConst ins = new InsBlckMethdConst();
+
+    }
+}
+

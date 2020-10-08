84661985db7c9ea0cbdb80ef772f3ae6a1384554
priya <priyankadeshpande034@gmail.com>
2020-10-08 20:43:42 +0530

    Exercise15

---
+++ src/Session54/Exercise15.java
@@ -0,0 +1,15 @@
+package Session54;
+
+public class Exercise15 {
+    static int i = 10;
+
+}
+ class Test17
+{
+    public static void main(String[] args) {
+        Exercise15 ex = new Exercise15();
+        System.out.println(ex.i);
+        System.out.println(Exercise15.i);
+
+    }
+}

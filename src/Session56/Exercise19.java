fca5453d58182a537d2daab89988eb8564a38250
priya <priyankadeshpande034@gmail.com>
2020-10-12 16:50:08 +0530

    Exercise19

---
+++ src/Session56/Exercise19.java
@@ -0,0 +1,11 @@
+package Session56;
+
+import static java.lang.Thread.*;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        System.out.println(MIN_PRIORITY);
+        System.out.println(NORM_PRIORITY);
+        System.out.println(MAX_PRIORITY);
+    }
+}

88ac442ddc6e4ac674ee20e2ab475db212cf9f6d
priya <priyankadeshpande034@gmail.com>
2020-10-12 15:39:15 +0530

    Exercise18

---
+++ src/Session56/Exercise18.java
@@ -0,0 +1,12 @@
+package Session56;
+
+public class Exercise18 {
+
+    static Object o = new Object() {
+        //Default Constuctor in Anonymous inner class
+        {
+            System.out.println("WELCOME To Durga Software Solutions");
+            System.exit(0);
+        }
+    };
+}

6c8fecd57d67a9c2a1b9b2b935da3f44fec48a58
priya <priyankadeshpande034@gmail.com>
2020-10-13 07:14:21 +0530

    Exercise21

--- src/Session58/Exercisse21.java
+++ src/Session58/Exercisse21.java
@@ -1,32 +1,32 @@
 package Session58;
 
 public class Exercisse21 {
-    static {
-        System.out.println("SB1 - A");
+    Exercisse21()
+    {
+        System.out.println("A-Con");
     }
-
-    static int i = m1();
-
-    static int m1() {
-        System.out.println("m1 - A");
+    static {
+        System.out.println("SB-A");
+    }
+    int m1(){
+        System.out.println("m1-A");
         return 10;
     }
-
-    static {
-        System.out.println("SB2 - A");
+    {
+        System.out.println("IB-A");
     }
-
-    static int j = m2();
-
-    static int m2() {
-        System.out.println("m2 - A");
+    static int m2(){
+        System.out.println("m2-A");
         return 20;
     }
-
+    static int i = m2();
+    int j = m1();
 }
 
 class Client21 {
     public static void main(String[] args) {
+
+
         Exercisse21 ex1 = new Exercisse21();
         Exercisse21 ex2 = new Exercisse21();
     }

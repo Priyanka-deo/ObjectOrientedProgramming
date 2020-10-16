91bc10998dc16e6896dbd7f4e6473bbf77924dd5
priya <priyankadeshpande034@gmail.com>
2020-10-16 15:07:05 +0530

    user

---
+++ src/Session61/User.java
@@ -0,0 +1,12 @@
+package Session61;
+//constant variables in java.
+public class User {
+    public static final int MIN_AGE = 18;
+    public static final int MAX_AGE = 25;
+}
+ class Client{
+     public static void main(String[] args) {
+         System.out.println(User.MIN_AGE);
+         System.out.println(User.MAX_AGE);
+     }
+}

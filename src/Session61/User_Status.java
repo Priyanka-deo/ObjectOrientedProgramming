75dba0730db979e852cc8906558a205fe93825d0
priya <priyankadeshpande034@gmail.com>
2020-10-17 09:21:38 +0530

    User_Status

---
+++ src/Session61/User_Status.java
@@ -0,0 +1,14 @@
+package Session61;
+
+public class User_Status {
+    public static final String AVAILABLE = "User Available";
+    public static final String BUSY = "User Busy";
+    public static final String IDLE = "User Idle";
+}
+ class Client28 {
+     public static void main(String[] args) {
+         System.out.println(User_Status.AVAILABLE);
+         System.out.println(User_Status.BUSY);
+         System.out.println(User_Status.IDLE);
+     }
+ }

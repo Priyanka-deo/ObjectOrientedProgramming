c396173ba5e80ae698dc18e54a1c3ebec3e4b2ac
priya <priyankadeshpande034@gmail.com>
2020-10-19 19:03:23 +0530

    Account,Employee2,Test31

---
+++ src/Session65/Test31.java
@@ -0,0 +1,19 @@
+package Session65;
+
+public class Test31 {
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.setAccNo("ABC123");
+        acc.setAccHolderName("Durga");
+        acc.setAccType("Saving");
+        acc.setBalance(25000);
+
+        Employee2 emp = new Employee2();
+        emp.setEno(111);
+        emp.setEname("Durga");
+        emp.setEsal(15000);
+        emp.setEaddr("Hyd");
+        emp.setAccount(acc);
+        emp.getEmpDetail();
+    }
+}
